fba8f15666c95d4720c7028c72bf9df8b5061600
bilalliberty <dominosliberty@gmail.com>
2014-01-16 22:24:22 +0300

    Revert change of ion heap, it causes problems

--- arch/arm/mach-msm/board-pyramid-display.c
+++ arch/arm/mach-msm/board-pyramid-display.c
@@ -841,7 +841,7 @@ static struct msm_panel_common_pdata mdp_pdata = {
 	.mdp_bus_scale_table = &mdp_bus_scale_pdata,
 #endif
 	.mdp_rev = MDP_REV_41,
-	.mem_hid = BIT(ION_CP_MM_HEAP_ID),
+	.mem_hid = BIT(ION_CP_WB_HEAP_ID),
 	.cont_splash_enabled = 0x00,
 	.splash_screen_addr = 0x00,
 	.splash_screen_size = 0x00,
