357bb50717262f3cb6d91c0c37ce0c6508950ce7
Ivan Grinko <iivanich@gmail.com>
2014-01-17 12:37:35 +0200

    Pyramid: Update panel

--- arch/arm/mach-msm/board-pyramid-display.c
+++ arch/arm/mach-msm/board-pyramid-display.c
@@ -1551,14 +1551,18 @@ err_device_put:
 }
 
 static struct mipi_dsi_phy_ctrl dsi_cmd_mode_phy_db = {
-       {0x03, 0x01, 0x01, 0x00},
-       {0x96, 0x1E, 0x1E, 0x00, 0x3C, 0x3C, 0x1E, 0x28, 0x0b, 0x13, 0x04},
-       {0x7f, 0x00, 0x00, 0x00},
-       {0xee, 0x02, 0x86, 0x00},
-       {0x41, 0x9c, 0xb9, 0xd6, 0x00, 0x50, 0x48, 0x63, 0x01, 0x0f, 0x07,
-        0x05, 0x14, 0x03, 0x03, 0x03, 0x54, 0x06, 0x10, 0x04, 0x03},
+/* DSI_BIT_CLK at 482MHz, 2 lane, RGB888 */
+                {0x03, 0x01, 0x01, 0x00},       /* regulator */
+                /* timing   */
+                {0xB4, 0x8D, 0x1D, 0x00, 0x20, 0x94, 0x20,
+                0x8F, 0x20, 0x03, 0x04},
+                {0x7f, 0x00, 0x00, 0x00},       /* phy ctrl */
+                {0xee, 0x02, 0x86, 0x00},       /* strength */
+                /* pll control */
+                {0x40, 0xf9, 0xb0, 0xda, 0x00, 0x50, 0x48, 0x63,
+                0x30, 0x07, 0x03,
+                0x05, 0x14, 0x03, 0x0, 0x0, 0x54, 0x06, 0x10, 0x04, 0x0},
 };
-
 
 static int __init mipi_cmd_novatek_blue_qhd_pt_init(void)
 {
@@ -1570,6 +1574,9 @@ static int __init mipi_cmd_novatek_blue_qhd_pt_init(void)
 	pinfo.pdest = DISPLAY_1;
 	pinfo.wait_cycle = 0;
 	pinfo.bpp = 24;
+
+	pinfo.width = 49;
+	pinfo.height = 87;
 	pinfo.lcdc.h_back_porch = 64;
 	pinfo.lcdc.h_front_porch = 96;
 	pinfo.lcdc.h_pulse_width = 32;
@@ -1577,15 +1584,16 @@ static int __init mipi_cmd_novatek_blue_qhd_pt_init(void)
 	pinfo.lcdc.v_front_porch = 16;
 	pinfo.lcdc.v_pulse_width = 4;
 
-        pinfo.lcd.primary_vsync_init = pinfo.yres;
-        pinfo.lcd.primary_rdptr_irq = 0;
-        pinfo.lcd.primary_start_pos = pinfo.yres +
-               pinfo.lcd.v_back_porch + pinfo.lcd.v_front_porch - 1;
-/*
 	pinfo.lcd.v_back_porch = 16;
 	pinfo.lcd.v_front_porch = 16;
 	pinfo.lcd.v_pulse_width = 4;
-*/
+
+        pinfo.lcd.primary_vsync_init = pinfo.yres;
+        pinfo.lcd.primary_rdptr_irq = 0;
+        pinfo.lcd.primary_start_pos = pinfo.yres +
+        pinfo.lcd.v_back_porch + pinfo.lcd.v_front_porch - 1;
+
+
 	pinfo.lcdc.border_clr = 0;
 	pinfo.lcdc.underflow_clr = 0xff;
 	pinfo.lcdc.hsync_skew = 0;
@@ -1597,7 +1605,7 @@ static int __init mipi_cmd_novatek_blue_qhd_pt_init(void)
 */
 	pinfo.lcd.vsync_enable = TRUE;
 	pinfo.lcd.hw_vsync_mode = TRUE;
-	pinfo.lcd.refx100 = 6200;
+	pinfo.lcd.refx100 = 6096;
 	pinfo.mipi.frame_rate = 60;
 	pinfo.mipi.mode = DSI_CMD_MODE;
 	pinfo.mipi.dst_format = DSI_CMD_DST_FORMAT_RGB888;
@@ -1606,8 +1614,8 @@ static int __init mipi_cmd_novatek_blue_qhd_pt_init(void)
 	pinfo.mipi.esc_byte_ratio = 4;
 	pinfo.mipi.data_lane0 = TRUE;
 	pinfo.mipi.data_lane1 = TRUE;
-	pinfo.mipi.t_clk_post = 0x0a;
-	pinfo.mipi.t_clk_pre = 0x1e;
+	pinfo.mipi.t_clk_post = 0x22;
+	pinfo.mipi.t_clk_pre = 0x3f;
 	pinfo.mipi.stream = 0;
 	pinfo.mipi.mdp_trigger = DSI_CMD_TRIGGER_NONE;
 	pinfo.mipi.dma_trigger = DSI_CMD_TRIGGER_SW;
