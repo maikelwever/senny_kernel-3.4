d26bc82dc5b67d10af6ec8f567df611dae829017
Ivan Grinko <iivanich@gmail.com>
2014-02-26 14:50:16 +0200

    Add to panel CMD_CLK_CTRL to fix backlight not turning on in some cases.

--- arch/arm/mach-msm/board-pyramid-display.c
+++ arch/arm/mach-msm/board-pyramid-display.c
@@ -1469,7 +1469,7 @@ static void pyramid_set_backlight(struct msm_fb_data_type *mfd)
 
 	cmdreq.cmds = novatek_cmd_backlight_cmds;
 	cmdreq.cmds_cnt = ARRAY_SIZE(novatek_cmd_backlight_cmds);
-	cmdreq.flags = CMD_REQ_COMMIT;
+	cmdreq.flags = CMD_REQ_COMMIT | CMD_CLK_CTRL;
 	cmdreq.rlen = 0;
 	cmdreq.cb = NULL;
 
@@ -1568,13 +1568,13 @@ static int __init mipi_cmd_novatek_blue_qhd_pt_init(void)
 	pinfo.lcdc.v_front_porch = 16;
 	pinfo.lcdc.v_pulse_width = 4;
 
-//	pinfo.lcd.v_back_porch = 16;
-//	pinfo.lcd.v_front_porch = 16;
-//	pinfo.lcd.v_pulse_width = 4;
+	pinfo.lcd.v_back_porch = 0;
+	pinfo.lcd.v_front_porch = 0;
+	pinfo.lcd.v_pulse_width = 0;
 
-        pinfo.lcd.primary_vsync_init = pinfo.yres;
-        pinfo.lcd.primary_rdptr_irq = 0;
-        pinfo.lcd.primary_start_pos = pinfo.yres +
+	pinfo.lcd.primary_vsync_init = pinfo.yres;
+	pinfo.lcd.primary_rdptr_irq = 0;
+	pinfo.lcd.primary_start_pos = pinfo.yres +
                pinfo.lcd.v_back_porch + pinfo.lcd.v_front_porch - 1;
 
 	pinfo.lcdc.border_clr = 0;
@@ -1584,7 +1584,7 @@ static int __init mipi_cmd_novatek_blue_qhd_pt_init(void)
 	pinfo.bl_min = 1;
 	pinfo.lcd.vsync_enable = TRUE;
 	pinfo.lcd.hw_vsync_mode = TRUE;
-	pinfo.clk_rate = 454000000;
+//	pinfo.clk_rate = 454000000;
 	pinfo.lcd.refx100 = 6200;
 	pinfo.mipi.frame_rate = 60;
 	pinfo.mipi.mode = DSI_CMD_MODE;
